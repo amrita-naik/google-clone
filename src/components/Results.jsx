2ea0499ef8af703b5fb61dc371cb7ec7220cab7f
amrita-naik <naikamrita254@gmail.com>
2022-02-25 21:54:00 +0530

    changes

---
+++ src/components/Results.jsx
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+import { useLocation } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+
+import { useResultContext } from '../contexts/ResultContextProvider'
+
+const Results = () => {
+
+  return (
+    <div>Results</div>
+  )
+}
+
+export default Results
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Results from './Results'
 
 const Search = () => {
   return (
-    <div>
-        Search
+    <div className='p-4'>
+        <Routes >
+          <Route path='/' element={<Navigate replace to="/search" />} />
+          <Route path='/search' element={<Results />} />
+          <Route path='/images' element={<Results />} />
+          <Route path='/news' element={<Results />} />
+          <Route path='/videos' element={<Results />} />
+        </Routes>
     </div>
   )
 }
